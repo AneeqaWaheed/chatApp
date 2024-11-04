f4fd38757c08bb952b78c9182923ee7780954fad
AneeqaWaheed <aneeqawaheed19@gmail.com>
2024-11-04 07:57:48 +0500

    Backend

---
+++ Backend/routes/user.route.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+import secureRoute from "../middleware/secureRoute.js";
+import {
+  allUsers,
+  login,
+  logout,
+  signup,
+} from "../controller/user.controller.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+router.get("/allusers", secureRoute, allUsers);
+
+export default router;
